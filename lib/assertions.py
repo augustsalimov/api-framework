3aa56922a522c9bbf50a89e18a4a3ca557ea0a75
Guseyn Salimov <maximweller@protonmail.com>
2022-01-16 14:08:29 +0300

    update lib

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,3 +11,37 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have a key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have a key {name}"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have a key {name}"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{name}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have a key {name}, but it is!"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected {expected_status_code}. Actual: {response.status_code}"
+
